4380ed606aca61bec97ec91b91c34751e3a0716a
Gyunay <gunai_sh29@mail.ru>
2023-12-26 10:39:23 +0300

    лаба 13

--- labs/Lab_13_python_oop/solution/base.py
+++ labs/Lab_13_python_oop/solution/base.py
@@ -1,12 +1,11 @@
 from abc import ABC, abstractmethod
 
 class BaseXlsBlock(ABC):
-    def __init__(self, worksheet, workbook, row, col, data):
-        self.worksheet = worksheet
-        self.workbook = workbook
-        self.row = row
-        self.col = col
-        self.data = data
+    ROW_INCREMENT = 12
+
+    def __init__(self, worksheet, row, col, data):
+        self.worksheet, self.row, self.col, self.data = worksheet, row, col, data
+
     @abstractmethod
     def write_header(self):
         pass
@@ -14,3 +13,6 @@ class BaseXlsBlock(ABC):
     @abstractmethod
     def write_data(self):
         pass
+
+    def get_row_increment(self):
+        return self.ROW_INCREMENT
