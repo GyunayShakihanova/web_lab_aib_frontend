1979a703469fc6c3c11b3e81eaf899ab4152e664
Gyunay <gunai_sh29@mail.ru>
2023-12-26 12:34:01 +0300

    12 исправлена

--- labs/Lab_12_python_intro/solutions/2/main.py
+++ labs/Lab_12_python_intro/solutions/2/main.py
@@ -1,24 +1,17 @@
-input_file = open('input.txt')
-input_text = input_file.read().split()
-Count_numbers = int(input_text[0])
-input_text.pop(0)
-input_text = [int (x) for x in input_text]
+import heapq
 
-def Sequence(sum_medians, sub_sequence_length, func_input_text):
-    temp_sequence = func_input_text[:sub_sequence_length]
-    temp_sequence.sort()
-    if len(temp_sequence) % 2 == 1:
-        if len(temp_sequence) == 1:
-            sum_medians += temp_sequence[0]
-        else:
-            sum_medians +=  temp_sequence[((len(temp_sequence) + 1) // 2) - 1]
-    elif len(temp_sequence) % 2 == 0:
-        sum_medians += temp_sequence[(len(temp_sequence) // 2) - 1]
-    sub_sequence_length += 1
-    if sub_sequence_length > Count_numbers:
-        return sum_medians
-    else:
-        return Sequence(sum_medians, sub_sequence_length, func_input_text)
+def find(n, arr):
+    maximum, minimum, result = [], [], 0
+    for i in range(n):
+        heapq.heappush(maximum, -arr[i])
+        heapq.heappush(minimum, -heapq.heappop(maximum))
 
-output_file = open('output.txt', 'w')
-output_file.write(str(Sequence(0, 1, input_text)))
+        if len(minimum) > len(maximum):
+            heapq.heappush(maximum, -heapq.heappop(minimum))
+        result -= maximum[0]
+
+    return result
+
+
+n, arr = int(input()), list(map(int, input().split()))
+print(find(n, arr))
